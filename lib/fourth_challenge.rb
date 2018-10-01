9dd146fc0f999607389f9c0fbd3a9a02433632e7
Merline Belizaire <merline@noukod.com>
2018-10-01 14:42:21 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,14 +1,13 @@
 def fourth_challenge
+
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
-        hash = {first: "first value!", second: "second value!"}
-
-        hash[:first]
+         {name: "Mercutio", age: "18", attitude: "hot-headed"},
+         {name: "Benvolio", age: "17", attitude: "worried"}
       ]
    },
    :capulet => {
@@ -16,13 +15,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
-        hash = {first: "first value!", second: "second value!"}
-
-       hash[:first]
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-}
+  }
 
 
 
